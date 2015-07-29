00299aaf272b9c74e44546b06a677415b4d65e93
Chris Patterson <pattersonc@ainfosec.com>
2015-07-29 13:26:54 -0400

    add command line parsing/options and add message handler
    Signed-off-by: Chris Patterson <pattersonc@ainfosec.com>

--- qtdbd/main.cpp
+++ qtdbd/main.cpp
@@ -1,29 +1,91 @@
 #include <QtCore/QCoreApplication>
 #include <QtCore/QTimer>
 #include <QtDBus/QtDBus>
+#include <QtGlobal>
+#include <stdio.h>
+#include <stdlib.h>
 #include "db.h"
 #include "db_adaptor.h"
 
+static bool debuggingEnabled = false;
+static bool domidLookupEnabled = false;
+static bool syslogEnabled = false;
+
+void logOutput(QtMsgType type, const QMessageLogContext&, const QString& msg)
+ {
+    /* TODO: honor syslog option */
+    switch (type) {
+    case QtDebugMsg:
+        if (debuggingEnabled) {
+            fprintf(stderr, "Debug: %s\n", qPrintable(msg));
+        }
+        break;
+    case QtInfoMsg:
+        fprintf(stderr, "Info: %s\n", qPrintable(msg));
+        break;
+    case QtWarningMsg:
+        fprintf(stderr, "Warning: %s\n", qPrintable(msg));
+        break;
+    case QtCriticalMsg:
+        fprintf(stderr, "Critical: %s\n", qPrintable(msg));
+        break;
+    case QtFatalMsg:
+        fprintf(stderr, "Fatal: %s\n", qPrintable(msg));
+        abort();
+    }
+ }
+
 int main(int argc, char *argv[])
 {
-   QCoreApplication app(argc, argv);
+    qInstallMessageHandler(logOutput);
+    QCoreApplication app(argc, argv);
+    QCoreApplication::setApplicationName("dbd");
+    QCoreApplication::setApplicationVersion("3.0");
 
-   if (!QDBusConnection::sessionBus().isConnected()) {
-       fprintf(stderr, "Cannot connect to the D-Bus system bus.\n");
-       return 1;
-   }
+    QCommandLineParser parser;
+    parser.setApplicationDescription("openxt simple db storage daemon");
+    parser.addHelpOption();
+    parser.addVersionOption();
 
-   if (!QDBusConnection::sessionBus().registerService("com.citrix.xenclient.db")) {
-       fprintf(stderr, "%s\n",
-               qPrintable(QDBusConnection::sessionBus().lastError().message()));
-       exit(1);
-   }
+    QCommandLineOption debugOption(QStringList() << "d" << "debug",
+            QCoreApplication::translate("main", "enable debug/verbose logging."));
+    parser.addOption(debugOption);
 
-   Db *db = new Db();
-   new DbInterfaceAdaptor(db);
+    QCommandLineOption lookupDomIDOption(QStringList() << "l" << "lookup-domid",
+            QCoreApplication::translate("main", "enable looking up sender domid using openxt specific call"));
+    parser.addOption(lookupDomIDOption);
 
-   QDBusConnection::sessionBus().registerObject("/", "com.citrix.xenclient.db", db, QDBusConnection::ExportAllSlots);
+    QCommandLineOption syslogOption(QStringList() << "s" << "syslog",
+            QCoreApplication::translate("main", "enable logging via syslog"));
+    parser.addOption(syslogOption);
 
-   printf("registered...\n");
-   app.exec();
+    QCommandLineOption maxDbFlushTimeOption(QStringList() << "t" << "max-db-flush-delay",
+            QCoreApplication::translate("main", "set maximum db flush delay to <milliseconds>."),
+            QCoreApplication::translate("main", "milliseconds"));
+    parser.addOption(maxDbFlushTimeOption);
+
+    parser.process(app);
+
+    debuggingEnabled = parser.isSet(debugOption);
+    domidLookupEnabled = parser.isSet(lookupDomIDOption);
+    syslogEnabled = parser.isSet(syslogOption);
+
+    if (!QDBusConnection::sessionBus().isConnected()) {
+        fprintf(stderr, "Cannot connect to the D-Bus system bus.\n");
+        return 1;
+    }
+
+    if (!QDBusConnection::sessionBus().registerService("com.citrix.xenclient.db")) {
+        fprintf(stderr, "%s\n",
+                qPrintable(QDBusConnection::sessionBus().lastError().message()));
+        exit(1);
+    }
+
+    Db *db = new Db();
+    new DbInterfaceAdaptor(db);
+
+    QDBusConnection::sessionBus().registerObject("/", "com.citrix.xenclient.db", db, QDBusConnection::ExportAllSlots);
+
+    qDebug("registered...\n");
+    app.exec();
 }
