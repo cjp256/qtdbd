c0f7cebd1dd3d69420a52cc6bfbfa57e598c8ea6
Chris Patterson <pattersonc@ainfosec.com>
2015-09-29 09:12:19 -0400

    set default arg for db-ls to /
    Signed-off-by: Chris Patterson <pattersonc@ainfosec.com>

--- app/db-ls/main.cpp
+++ app/db-ls/main.cpp
@@ -74,6 +74,7 @@ void parseCommandLine(QCommandLineParser &parser, QCoreApplication &app, CmdLine
     opts->debuggingEnabled = false;
     opts->syslogEnabled = false;
     opts->sessionBusEnabled = false;
+    opts->key = QString("/");
 
     parser.setApplicationDescription("openxt simple db storage daemon");
     parser.addHelpOption();
@@ -100,12 +101,9 @@ void parseCommandLine(QCommandLineParser &parser, QCoreApplication &app, CmdLine
     opts->sessionBusEnabled = parser.isSet(sessionBusOption);
 
     const QStringList posArgs = parser.positionalArguments();
-    if (posArgs.size() < 1) {
-        qWarning() << "invalid arguments";
-        exit(1);
-    }
-
-    opts->key = posArgs.at(0);
+    if (posArgs.size() >= 1) {
+        opts->key = posArgs.at(0);
+    }    
 
     qDebug() << "debugging enabled:" << opts->debuggingEnabled;
     qDebug() << "syslog enabled:" << opts->syslogEnabled;
